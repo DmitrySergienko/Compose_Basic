6bd5c6344d89a6c71ebd45e8a160d38660a36725
DmitrySergienko <dmitrisergienko@gmail.com>
2022-06-16 12:16:37 +0400

    7.styling (animation upd)

--- app/src/main/java/com/example/composebasic/MainActivity.kt
+++ app/src/main/java/com/example/composebasic/MainActivity.kt
@@ -4,15 +4,22 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.coerceAtLeast
@@ -34,10 +41,10 @@ class MainActivity : ComponentActivity() {
     fun MyApp() {
 
         // TODO: This state should be hoisted
-        var shouldShowOnboarding by rememberSaveable{ mutableStateOf(true) }
+        var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
         if (shouldShowOnboarding) {
-                OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
         } else {
             Greetings()
 
@@ -60,15 +67,23 @@ class MainActivity : ComponentActivity() {
     private fun Greeting(name: String) {
         var expanded by remember { mutableStateOf(false) }
         val extraPadding by animateDpAsState(
-            targetValue = if (expanded) 250.dp else 0.dp,
-            animationSpec = tween(2000)
+            targetValue = if (expanded) 50.dp else 0.dp,
+            //animationSpec = tween(1000)
         )
 
         Surface(
             color = MaterialTheme.colors.primary,
             modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
         ) {
-            Row(modifier = Modifier.padding(24.dp)) {
+            Row(modifier = Modifier
+                .padding(24.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
+            ) {
 
                 Column(
                     modifier = Modifier
@@ -77,14 +92,33 @@ class MainActivity : ComponentActivity() {
                 ) {
 
                     Text(text = "Hello, ")
-                    Text(text = name, style = MaterialTheme.typography.h4.copy(
-                        fontWeight = FontWeight.ExtraBold
-                    ))
+                    Text(
+                        text = name, style = MaterialTheme.typography.h4.copy(
+                            fontWeight = FontWeight.ExtraBold
+                        )
+                    )
+                    if (expanded) {
+                        Text(
+                            text = ("Composem ipsum color sit lazy, " +
+                                    "padding theme elit, sed do bouncy. ").repeat(4),
+                        )
+                    }
                 }
-                OutlinedButton(onClick = { expanded = !expanded })
-                {
-                    Text(if (expanded) "Show more" else "show less")
+
+                IconButton(
+                    onClick = { expanded = !expanded },
+                ) {
+                    Icon(
+                        imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                        contentDescription = if (expanded) {
+                            stringResource(R.string.app_name)
+                        } else {
+                            stringResource(R.string.app_name)
+                        }
+                    )
+                    //Text(if (expanded) "Show more" else "show less")
                 }
+
             }
         }
     }
